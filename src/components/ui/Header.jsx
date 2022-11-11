521800a0f6d3e1858e4d3cac0081a9cfe952ea8c
prkm93 <aradshub93@gmail.com>
2022-11-11 20:43:17 +0530

    added header

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AppBar, Toolbar, useScrollTrigger } from "@material-ui/core";
+import { AppBar, Toolbar, useScrollTrigger, Tabs, Tab, Button } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 import logo from "../../assets/logo.svg";
@@ -26,6 +26,21 @@ const useStyles = makeStyles(theme => ({
   },
   logo: {
     height: "7em"
+  },
+  tabContainer: {
+    marginLeft: "auto"
+  },
+  tab: {
+    ...theme.typography.tab,
+    minWidth: 10,
+    marginLeft: "25px" // here pixel is used because we want the spacing between tabs same irrespective of the device resolution
+  },
+  button: {
+    ...theme.typography.estimate,
+    borderRadius: "50px",
+    marginRight: "25px",
+    marginLeft: "50px",
+    height: "45px"
   }
 }))  
  
@@ -39,6 +54,14 @@ const Header = (props) => {
          <AppBar position="fixed">
             <Toolbar disableGutters>
               <img src={logo} alt="company logo" className={classes.logo}/>
+              <Tabs className={classes.tabContainer}>
+                <Tab className={classes.tab} label="Home"/>
+                <Tab className={classes.tab} label="Services"/>
+                <Tab className={classes.tab} label="The Revolution"/>
+                <Tab className={classes.tab} label="About Us"/>
+                <Tab className={classes.tab} label="Contact Us"/>
+              </Tabs>
+              <Button variant="contained" color="secondary" className={classes.button}>Free Estimate</Button>
             </Toolbar> 
          </AppBar>
         </ElevationScroll>
